5d5f2554030f938f6df4d310ee01d3752880a143
2021salvatoribarconjoaquin-arch <2021salvatoribarconjoaquin@colegioubaescobar.gob.ar>
2026-07-13 15:35:42 -0300

    Update descargadorrrr.app.py

--- descargadorrrr.app.py
+++ descargadorrrr.app.py
@@ -27,6 +27,10 @@ def completar(numero):
 
     return redirect(url_for("inicio"))
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
-# deploy fix
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
